cec12316f647869519fc0e9723150714e3377b33
Joshua Garland <joshuag@choracapital.com>
2017-10-25 16:57:01 +0200

    updating min working example

--- min_working_example.py
+++ min_working_example.py
@@ -3,5 +3,10 @@ import numpy as np
 
 
 TS = np.random.rand(50000)
+
 wpe = np.asarray(ordinal_TSA.permutation_entropy(TS.reshape(len(TS),1),dim=3,step=1,w=1))
+print(wpe)
+TS = np.ones(50000)
+wpe = np.asarray(ordinal_TSA.permutation_entropy(TS.reshape(len(TS),1),dim=3,step=1,w=1))
+
 print(wpe)
